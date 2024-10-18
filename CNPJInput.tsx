be8b6257b77b73bdf763c1fbb3e26671c22a1617
Kauê Henrick <kauehenrick@proton.me>
2024-10-18 16:17:21 -0300

    first commit

---
+++ CNPJInput.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useReducer } from "react";
+import { UseFormReturn } from "react-hook-form";
+
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormDescription,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PiMagnifyingGlass } from "react-icons/pi";
+
+type TextInputProps = {
+    form: UseFormReturn<any>;
+    name: string;
+};
+
+function cnpj(v: string) {
+    v = v.replace(/\D/g, "");
+    v = v.replace(/^(\d{2})(\d)/, "$1.$2");
+    v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+    v = v.replace(/\.(\d{3})(\d)/, ".$1/$2");
+    v = v.replace(/(\d{4})(\d)/, "$1-$2");
+    return v;
+}
+
+export default function CNPJInput(props: TextInputProps) {
+    const initialValue = props.form.getValues()[props.name] || "";
+
+    const [value, setValue] = useReducer((_: any, next: string) => {
+        return cnpj(next);
+    }, initialValue);
+
+    function handleChange(realChangeFn: Function, formattedValue: string) {
+        const digits = formattedValue.replace(/\D/g, "");
+        realChangeFn(digits);
+    }
+
+    return (
+        <FormField
+            control={props.form.control}
+            name={props.name}
+            render={({ field }) => {
+                const _change = field.onChange;
+
+                return (
+                    <FormItem className="space-y-3">
+                        <FormLabel className="flex items-center justify-between mt-1">
+                            <p>CNPJ</p>
+                            <div className="flex items-center gap-2 text-blue-500 cursor-pointer">
+                                <p className="text-xs">Buscar dados</p>
+                                <PiMagnifyingGlass />
+                            </div>
+                        </FormLabel>
+                        <FormControl>
+                            <Input
+                                placeholder=""
+                                type="text"
+                                maxLength={18}
+                                {...field}
+                                value={value}
+                                onChange={(ev) => {
+                                    const newValue = ev.target.value;
+                                    setValue(newValue);
+                                    handleChange(_change, newValue);
+                                }}
+                                className='w-[220px]'
+                            />
+                        </FormControl>
+                        <FormDescription className="hidden" />
+                        <FormMessage />
+                    </FormItem>
+                );
+            }}
+        />
+    );
+}
